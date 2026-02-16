9258ba2cb5dfe1b0a877627e4ba577fbcba89f93
khoirinnisaamaliapplg-glitch <khoirinnisaamaliapplg@gmail.com>
2026-02-16 20:03:08 +0700

    baru

--- software/Chords-Web/next.config.mjs
+++ software/Chords-Web/next.config.mjs
@@ -1,29 +1,10 @@
-import nextPwa from "next-pwa";
-
 /** @type {import('next').NextConfig} */
-const withPWA = nextPwa({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-});
-
-const isGithubActor = process.env.NEXT_PUBLIC_GITHUB_ACTOR === "upsidedownlabs" || process.env.NEXT_PUBLIC_GITHUB_ACTOR === undefined
-
-const config = {
+const nextConfig = {
+  output: "export",
   reactStrictMode: true,
-  output: "export", // Ensures it works with static export
-  basePath: isGithubActor ? "" : "/Chords-Web",
-  assetPrefix: isGithubActor ? "" : "/Chords-Web",
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
   },
-  ...withPWA,
 };
 
-export default config;
+export default nextConfig;
